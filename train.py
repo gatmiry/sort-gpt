7134306b09f3a30d791bfbcd7bf6402f9bb6033f
Khashayar Gatmiry <khashayar@Khashayars-MacBook-Pro.local>
2025-03-20 15:12:08 -0400

    train file added

---
+++ train.py
@@ -0,0 +1,77 @@
+import torch
+block_size = 1024
+batch_size = 8
+import torch
+import numpy as np
+import os
+def get_batch(split):
+    dtype = np.uint16
+    data = np.memmap(os.path.join(os.getcwd(), f'{split}.bin'), dtype=dtype, mode='r')
+    #print('data is ', type(data))
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+    return x, y
+
+import math
+warmup_iters = 2000
+max_iters = 600000
+learning_rate = 6e-4
+min_lr = 6e-5
+decay_lr = True
+def get_lr(itr):
+    if itr < warmup_iters:
+       return learning_rate * (itr + 1) / (warmup_iters + 1)
+    if itr > max_iters:
+       return min_lr
+    assert warmup_iters <= itr <= max_iters, f'itr is out of bound'
+    ratio = (itr - warmup_iters) / (max_iters - warmup_iters)
+    ratio = 0.5 * (1.0 + math.cos(math.pi * ratio))
+    lr = min_lr + ratio * (learning_rate - min_lr)
+    return lr
+
+
+def create_optimizer(model, weight_decay, learning_rate, device):
+   params = [p for p in model.parameters() if p.requires_grad]
+   decay_params = [p for p in params if p.dim() > 1]
+   nondecay_params = [p for p in params if p.dim() <= 1]
+   optim_groups = [
+      {'params': decay_params, 'weight_decay': weight_decay},
+      {'params': nondecay_params, 'weight_decay': 0.0}
+   ]
+   num_decay_params = sum(p.numel() for p in decay_params)
+   num_nondecay_params = sum(p.numel() for p in nondecay_params)
+   print(f'num decay params: {num_decay_params} num nondecay params: {num_nondecay_params}')
+   import inspect
+   fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+   use_fused = fused_available and 'cuda' in device
+   print(f'using fused Adam: {use_fused}')
+   optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=False)
+   return optimizer
+
+
+
+
+from model import GPT, GPTConfig
+
+mymodel = GPT(GPTConfig())
+device = 'cpu'
+if torch.cuda.is_available():
+   device = 'cuda'
+elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+   device = 'mps'
+print(f'using device: {device}')
+
+
+optimizer = create_optimizer(mymodel, weight_decay=0.1, learning_rate=6e-4, device=device)
+max_iter = 10000
+for itr in range(max_iter):
+   optimizer.zero_grad()
+   x,y = get_batch('train')
+   logits, loss = mymodel(x,y)
+   loss.backward()
+   lr = get_lr(itr)
+   for param_group in optimizer.param_groups:
+      param_group['lr'] = lr
+   optimizer.step()
+   print(f'loss: {loss.item()}')
